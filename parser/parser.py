7acb974e29c545a41b1d7c5c129f479da4be256c
Florian Magot <flo.magot@gmail.com>
2015-03-07 02:14:59 +0100

    Updated parser for directors

--- parser/parser.py
+++ parser/parser.py
@@ -1,104 +1,108 @@
 import re
 import time
-
+import sys
 from py2neo import Graph
 from py2neo import Node, Relationship
+from py2neo.packages.httpstream import http
+
+
+if len(sys.argv) < 2:
+  print "Please specify the data to parse"
+  print "For example: python parser.py value"
+  print "Values: all, movies, directors"
+  exit()
+
+http.socket_timeout = 9999
+graph = Graph()
+globalStart = time.time()
 
 # Encode string for cypher query
 def encodeName(str):
   str = unicode(str, encoding='latin-1')
   str = str.replace('\\', '\\\\')
   str = str.replace('\"', '\\\"')
-  return;
+  return str
+
+# Add a query to the current transaction
+def executeTransaction(count, tx, query, steps):
+  if query:
+    count += 1
+    tx.append(query)
+    if steps == 0 or count % steps == 0:
+      print count
+      start = time.time()
+      tx.commit()
+      end = time.time()
+      print str(steps) + " elements inserted in " + str(end - start) + " seconds"
+      tx = graph.cypher.begin()
+  return count, tx
 
 
-graph = Graph()
-
-# print "parsing movies..."
-# count = 0;
-# with open("movies.list") as f:
-#   for _ in xrange(15):
-#     next(f)
-#   tx = graph.cypher.begin()
-#   for line in f:
-
-#     # skipping invalid line
-#     if not line or "(TV)" in line or line[0] == "\"" or "(V)" in line:
-#       continue
-#     values = re.split(r'\t+', line)
-#     if len(values) < 2:
-#       continue
-
-#     # getting values
-#     title = unicode(values[0], encoding='latin-1')[:-7]
-#     title = title.replace('\\', '\\\\')
-#     title = title.replace('\"', '\\\"')
-#     year = values[1][:4]
-
-#     # creating request
-#     if '{' in title or year[0] == "?":
-#       continue
-#     query = 'CREATE (:Movie {title:\"' + title + '\", released:' + year + '})\n'
-#     tx.append(query)
-#     count += 1
-#     if count % 25000 == 0:
-#       print count
-#       start = time.time()
-#       tx.commit()
-#       end = time.time()
-#       print "Elements inserted in " + str(end - start) + " seconds"
-#       tx = graph.cypher.begin()
-#   tx.commit()
-
-
-
-print "parsing directors..."
-personId = 0
-count = 0;
-with open("directors.list") as f:
-  
-  #skipping head of file
-  line = next(f)
-  while line != "THE DIRECTORS LIST\n":
-    line = next(f)
-  for _ in xrange(5):
-    next(f)
-
-  tx = graph.cypher.begin()
-  for line in f:
-  	
-    # skipping invalid lines
-    if not line:
-      continue
-    if line.startswith("----"):
-      break
-    values = re.split(r'\t+', line)
-    if len(values) < 2:
-      continue
-
-    # getting values
-    name = encodeName(values[0])
-    film = encodeName(values[1][:-8])
-
-    # creating the person 
-    if name:
-      query = 'CREATE (_'  + str(personId) + ':Person {name:"' + name + '"})'
-      tx.append(query)
-      personId += 1
-      count += 1
-
-    # creating the relationships
-    if film:
-      if '{' in film or '(????)' in film or "(TV)" in film or "(V)" in film:
+# movies
+if sys.argv[1] == "all" or sys.argv[1] == "movies":
+  print "parsing movies..."
+  count = 0
+  steps = 25000
+  with open("movies.list") as f:
+    # skipping head of file
+    for _ in xrange(15):
+      next(f)
+    tx = graph.cypher.begin()
+    for line in f:
+      # skipping invalid line
+      if not line or "(TV)" in line or line[0] == "\"" or "(V)" in line:
         continue
-      query = 'CREATE UNIQUE (_' + str(personId - 1) +')-[:DIRECTED]->(:Movie {title:"' + film +'"})'
-      tx.append(query)
-      count += 1
-      if count % 25000 == 0:
-        print count
-        start = time.time()
-        tx.commit()
-        end = time.time()
-        print "Elements inserted in " + str(end - start) + " seconds"
-        tx = graph.cypher.begin()
-  tx.commit()
+      values = re.split(r'\t+', line)
+      if len(values) < 2:
+        continue
+      # getting values
+      title = encodeName(values[0][:-7])
+      year = values[1][:4]    
+      if '{' in title or year[0] == "?":
+        continue
+      # creating and executing request
+      query = 'CREATE (:Movie {title:\"' + title + '\", released:' + year + '})\n'
+      count, tx = executeTransaction(count, tx, query, steps)
+    tx.commit()
+
+
+# directors
+if sys.argv[1] == "all" or sys.argv[1] == "directors":
+  print "parsing directors..."
+  count = 0
+  query = ""
+  steps = 5000
+  with open("directors.list") as f:
+    #skipping head of file
+    line = next(f)
+    while line != "THE DIRECTORS LIST\n":
+      line = next(f)
+    for _ in xrange(5):
+      next(f)
+    tx = graph.cypher.begin()
+    for line in f:
+      # skipping invalid lines
+      if not line:
+        continue
+      if line.startswith("----"):
+        break
+      values = re.split(r'\t+', line)
+      if len(values) < 2:
+        continue
+      # getting values
+      name = encodeName(values[0])
+      film = values[1]
+      if '{' in film or '(????)' in film or "(TV)" in film or "(V)" in film:
+        film = ""
+      else:
+        film = encodeName(film[:-8])
+      # creating the person and the relationships
+      if name:
+        count, tx = executeTransaction(count, tx, query, steps)
+        query = 'CREATE (person:Person {name:\"' + name + '\"})'
+      if film:
+        query += '\nCREATE UNIQUE (person)-[:DIRECTED]->(:Movie {title:"' + film +'"})'
+    count, tx = executeTransaction(count, tx, query, 0)
+    tx.commit()
+
+print str((time.time() - globalStart) / 60) + " minutes total execution time"

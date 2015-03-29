a9f4debf6279b59fb44359d9fd9d352b1bc84aae
Jean Reptile <jeanreptile@gmail.com>
2015-03-29 03:36:01 +0200

    Set style of search bar.

--- cinegraph/public/javascripts/search.js
+++ cinegraph/public/javascripts/search.js
@@ -1,10 +1,17 @@
 angular.module('cinegraphApp').controller('TypeaheadCtrl', function($scope, $http) {
-    console.log('btsad');
-          console.log($('#spanTest').val($(this).text()));
+
+    $scope.selectedItem = "movie";
+
+    $scope.OnItemClick = function(event) {
+        $scope.selectedItem = event;
+    }
+
+    console.log($('#spanTest').val($(this).text()));
     $('#selectType li').on('click', function(){
         console.log("test");
-          console.log($('#spanTest').val($(this).text()));
-  });
+        console.log($('#spanTest').val($(this).text()));
+        $scope.test23 = "merde";
+    });
 
   $scope.getLocation = function(val) {
       console.log('val is');

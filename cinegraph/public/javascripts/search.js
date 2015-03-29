a9f751a2c96c418be401a6c906d68072de78a949
Jean Reptile <jeanreptile@gmail.com>
2015-03-29 05:03:52 +0200

    Cover background image + blurring when search bar is focused.

--- cinegraph/public/javascripts/search.js
+++ cinegraph/public/javascripts/search.js
@@ -1,5 +1,19 @@
 angular.module('cinegraphApp').controller('TypeaheadCtrl', function($scope, $http) {
 
+
+  var vague = $('#bg').Vague({
+    intensity:      20,      // Blur Intensity
+    forceSVGUrl:    false,   // Force absolute path to the SVG filter,
+    // default animation options
+  });
+//vague.blur();
+
+$("#searchFormControl").on("click",$.proxy(vague.toggleblur,vague));
+
+//$('#searchFormControl').prop('disabled', $.proxy(vague.toggleblur,vague));
+
+
+
     $scope.selectedItem = "movie";
 
     $scope.OnItemClick = function(event) {

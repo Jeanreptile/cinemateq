0a809e7c8c3ccd4d41411ac0990a961cabf8ff45
Jean Reptile <jeanreptile@gmail.com>
2015-04-24 21:07:28 +0200

    Not using passport anymore

--- cinegraph/passport/login.js
+++ cinegraph/passport/login.js
@@ -26,7 +26,8 @@ passport.use('login', new LocalStrategy({
         // User and password both match, return user from
         // done method which will be treated like success
       }
-      return done(null, objs[0]);
+      user = {'firstName':objs[0].firstName, 'lastName':objs[0].lastName, 'username': objs[0].username, 'id':objs[0].id}
+      return done(null, user);
       });
 }));
     var isValidPassword = function(user, password){
